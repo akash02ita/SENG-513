7392b979c9463be3df4957409d0f8578765a88c1
Akashdeep Singh <akash02ita@gmail.com>
2022-11-11 01:52:09 -0700

    a2: lines click and rendering working By this point clicking where dashed line is shown will create a line. Also fixed the bug of floating point inaccuracies, where needed. Such slight inaccuracies caused lines to be drawn in wrong place or too small to be visible, due to Math.floor when converting coordinates to points in grid system. Rounding to 1 decimal place where needed and staying consistent in that allowed to fix the bug. By this point a similar issue should not arise again.

--- a2/dots-and-boxes/src/components/Game.js
+++ a2/dots-and-boxes/src/components/Game.js
@@ -5,11 +5,14 @@ import { useEffect, useLayoutEffect, useRef, useState } from 'react';
 function Game(props) {
 
     // get size of board (dots*dots) otherwise by default 4x4 dots
-    const rows = props.rows && props.cols ? props.rows : 4;
-    const cols = props.rows && props.cols ? props.cols : 4;
+    const rows = props.rows && props.cols && props.rows > 1 && props.cols > 1 && props.rows < 10 && props.cols < 10 ? props.rows : 4; // number of dots horizontally
+    const cols = props.rows && props.cols && props.rows > 1 && props.cols > 1 && props.rows < 10 && props.cols < 10 ? props.cols : 4; // number of dots vertically
+    const playerColors = ["red", "green", "blue", "yellow", "orange", "pink", "purple"];
+    const totalLines = (rows - 1) * (cols - 1);
+    const maxPlayerCount = Math.min(totalLines, playerColors.length);
 
     // get playerCount otherwise by default 3
-    const playerCount = props.playerCount && props.playerCount > 1 ? props.playerCount : 3;
+    const playerCount = props.playerCount && props.playerCount > 1 ? Math.min(maxPlayerCount, props.playerCount) : 3;
 
     /*
         The following 2 sources helped me figure how to retrieve dimensions without 'infinite' loop issues.
@@ -30,6 +33,8 @@ function Game(props) {
                 width: divRef.current.offsetWidth,
                 height: divRef.current.offsetHeight
             });
+            // remove dashed line on resize
+            setDashedLineCoord({ x1: null, y1: null, x2: null, y2: null, });
         };
 
         window.addEventListener("resize", handleResize);
@@ -44,17 +49,24 @@ function Game(props) {
     // this method allows to find which line is closest (the line to be dashed or to be clicked)
     const [dashedLineCoord, setDashedLineCoord] = useState({ x1: null, y1: null, x2: null, y2: null });
     const getClosestLineCoordByMouse = () => {
+        console.log("BEGIN getClosestLineCoordByMouse");
         // invalid mouse coordinates output null coord entries
         if (mouseCoord.x < 0 || mouseCoord.x > dimensions.width) {
+            console.log("ERR getClosestLineCoordByMouse: Invalid x coordinates");
             return [null, null, null, null];
         }
         if (mouseCoord.y < 0 || mouseCoord.y > dimensions.height) {
+            console.log("ERR getClosestLineCoordByMouse: Invalid y coordinates");
             return [null, null, null, null];
         }
 
         // Do calculations in Local Space
         const [idealWidth, idealHeight, offsetX, offsetY] = idealFittingSize();
+        console.log("getClosestLineCoordByMouse: [idealWidth, idealHeight, offsetX, offsetY]");
+        console.log([idealWidth, idealHeight, offsetX, offsetY]);
         const [x, y] = [Math.max(0, mouseCoord.x - offsetX), Math.max(0, mouseCoord.y - offsetY)];
+        console.log("getClosestLineCoordByMouse: [x, y] -offset for local space");
+        console.log([x, y]);
         let closestX1 = Math.min(
             idealWidth / (cols - 1) * (cols - 2),
             Math.floor(x / idealWidth * (cols - 1)) / (cols - 1) * idealWidth);
@@ -64,12 +76,17 @@ function Game(props) {
         let closestX2 = closestX1 + idealWidth / (cols - 1);
         let closestY2 = closestY1 + idealHeight / (rows - 1);
 
+        console.log("getClosestLineCoordByMouse: before global space: [closestX1,Y1,X2,y2");
+        console.log([closestX1, closestY1, closestX2, closestY2]);
+
         // go back to Global Space
         closestX1 += offsetX;
         closestY1 += offsetY;
         closestX2 += offsetX;
         closestY2 += offsetY;
 
+        console.log("getClosestLineCoordByMouse: after global space: [closestX1,Y1,X2,y2");
+        console.log([closestX1, closestY1, closestX2, closestY2]);
         // Now we have 2 x and 2 y coordinates whose cartesia product will yield to the 4 points
         // Those 4 points EXACTLY represent the box in which the line is present
         // The task is now to see where and which line is closest within that box according to mouse coordinates
@@ -128,23 +145,84 @@ function Game(props) {
         const C = [closestX2, closestY2];
         const D = [closestX1, closestY2];
 
-        const [lineX1, lineY1, lineX2, lineY2] = getLineByTriangleContainingPoint(E, A, B, C, D);
-        console.log("Debug getlinebymouse");
+        const [lineX1, lineY1, lineX2, lineY2] = getLineByTriangleContainingPoint(E, A, B, C, D).map((num) => dPR(num * 10) / 10);
+        console.log("getClosestLineCoordByMouse: FINAL OUTPUT: [lineX1, lineY1, lineX2, lineY2]");
         console.log([lineX1, lineY1, lineX2, lineY2]);
         return [lineX1, lineY1, lineX2, lineY2];
     }
 
     const getClosestLinePointsByMouse = () => {
+        console.log("BEGIN getClosestLinePointsByMouse");
+        let [lineX1, lineY1, lineX2, lineY2] = getClosestLineCoordByMouse();
+        console.log("back to getClosestLinePointsByMouse");
+        // convert line coordinates to point coordinates
+        if (lineX1 && lineY1 && lineX2 && lineY2) {
+            console.log("getClosestLinePointsByMouse: successful entering of if");
+            // PROBLEM: java precision floating is messing up at edge cases. example: Math.floor(0.9999/1.0) = 0 instead of 1
+            // [lineX1, lineY1, lineX2, lineY2] = [lineX1, lineY1, lineX2, lineY2].map((num) => dPR(num);
+            // TODO: do calculations in local space first
+            const [idealWidth, idealHeight, offsetX, offsetY] = idealFittingSize();
+
+            console.log("getClosestLinePointsByMouse: [idealWidth, idealHeight, offsetX, offsetY]");
+            console.log([idealWidth, idealHeight, offsetX, offsetY]);
+
+            lineX1 = Math.max(0, dPR(lineX1 - offsetX));
+            lineY1 = Math.max(0, dPR(lineY1 - offsetY));
+            lineX2 = Math.max(0, dPR(lineX2 - offsetX));
+            lineY2 = Math.max(0, dPR(lineY2 - offsetY));
+            console.log("getClosestLinePointsByMouse: offset to local space: [lineX1, lineY1, lineX2, lineY2]");
+            console.log([lineX1, lineY1, lineX2, lineY2]);
+
+            const x1 = Math.floor(lineX1 / idealWidth * (cols - 1));
+            const y1 = Math.floor(lineY1 / idealHeight * (rows - 1));
+            const x2 = Math.floor(lineX2 / idealWidth * (cols - 1));
+            const y2 = Math.floor(lineY2 / idealHeight * (rows - 1));
+            console.log("getClosestLinePointsByMouse: FINAL OUPTUT: [x1, y1, x2, y2]");
+            console.log([x1, y1, x2, y2]);
+            return [x1, y1, x2, y2];
+        }
+
+        // return null if invalid mouse coordinates (or outside all boxes)
+        console.log("FAILED getClosestLinePointsByMouse: output null");
+        return [null, null, null, null];
 
     }
-    const getLineCoordBetweenPoints = () => {
+    const getLineCoordBetweenPoints = ([x1, y1, x2, y2]) => {
+        console.log("BEGIN getLineCoordBetweenPoints");
+        console.log("INPUT: [x1, y1, x2, y2]");
+        console.log([x1, y1, x2, y2]);
+        // ensure x \in [mod rows] and y \in [mod cols]
+        if (x1 < 0 || y1 < 0 || x2 < 0 || y2 < 0) {
+            console.log("getLineCoordBetweenPoints: INVALID < 0");
+            return [null, null, null, null];
+        }
+        if (x1 > cols - 1 || y1 > rows - 1 || x2 > cols - 1 || y2 > rows - 1) {
+            console.log("getLineCoordBetweenPoints: INVALID > limitsize");
+            return [null, null, null, null];
+        }
 
+        // Coordinates must be calculated in local space than offset to global
+        const [idealWidth, idealHeight, offsetX, offsetY] = idealFittingSize();
+        console.log("getLineCoordBetweenPoints: [idealWidth, idealHeight, offsetX, offsetY]");
+        console.log("[idealWidth, idealHeight, offsetX, offsetY]");
+        let lineX1 = x1 / (cols - 1) * idealWidth + offsetX;
+        let lineY1 = y1 / (rows - 1) * idealHeight + offsetY;
+        let lineX2 = x2 / (cols - 1) * idealWidth + offsetX;
+        let lineY2 = y2 / (rows - 1) * idealHeight + offsetY;
+
+        console.log("getLineCoordBetweenPoints: FINAL STAGE");
+        console.log("INPUT:  [x1, y1, x2, y2]");
+        console.log([x1, y1, x2, y2]);
+        console.log("OUTPUT: [lineX1, lineY1, lineX2, lineY2]");
+        console.log([lineX1, lineY1, lineX2, lineY2]);
+        // return line coordnates, in Global Space
+        return [lineX1, lineY1, lineX2, lineY2];
     }
 
 
     /*
         The TA source of event listener and the following sources helped me figure out and ensure I am properly getting mouse coordinates during mouse movement and click.
-
+    
         https://codingbeautydev.com/blog/react-get-mouse-position/
     */
     const [mouseCoord, setMouseCoord] = useState({ x: 0, y: 0 });
@@ -169,6 +247,7 @@ function Game(props) {
     }
 
     const handleMouseClick = (event) => {
+        console.log("check1");
         setMouseCoord({
             x: event.pageX - document.getElementById(props.boardId).offsetLeft,
             y: event.pageY - document.getElementById(props.boardId).offsetTop,
@@ -177,12 +256,50 @@ function Game(props) {
         // console.log(mouseCoord);
         // console.log(event);
 
+        /* 
+        TODO: UPDATE:
+        - turn: which is a numberic number \in [mod playerCount]
+        - lines to draw
+        - make a list of colors
+        - draw line with color = colors[turn]
+        - score
+        - history
+        */
+        console.log("check2");
+        // get clone of dictionary
+        const currentStatus = history[history.length - 1];
+        const newClickedLines = { ...currentStatus.clickedLines };
+
+        const [x1, y1, x2, y2] = getClosestLinePointsByMouse();
+        if ([x1, y1, x2, y2].includes(null)) {
+            return;
+        }
+        const key = [x1, y1, x2, y2].join('-');
+        // do not allow to click previously clicked lines
+        if (key in newClickedLines) {
+            return;
+        }
+
+        newClickedLines[key] = currentStatus.turn;
+        const newPlayesScore = { ...currentStatus.playersScore };
+
+        // TODO: score increases only if a box is completed
+        newPlayesScore[currentStatus.turn] += 1;
+        const newTurn = (currentStatus.turn + 1) % playerCount;
+
+        setHistory(history.concat({
+            clickedLines: newClickedLines,
+            playersScore: newPlayesScore,
+            turn: newTurn,
+        }))
     }
 
     // HISTORY to keep track of all moves. Also allows to go back in history or restart game
+    const firstTurn = props.firstTurn ? props.firstTurn : 0;
     const [history, setHistory] = useState([{
         clickedLines: {},
         playersScore: generatePlayersZeroScore(playerCount),
+        turn: firstTurn,
     }]);
     // console.log("Debug history");
     // console.log(history);
@@ -197,8 +314,10 @@ function Game(props) {
     const idealFittingSize = () => {
         const idealHeight = Math.max(currHeight * SCALE_FACTOR, currHeight - SCALE_TRANSLATION);
         const idealWidth = Math.max(currWidth * SCALE_FACTOR, currWidth - SCALE_TRANSLATION);
-        const offsetX = (currWidth - idealWidth) / 2;
-        const offsetY = (currHeight - idealHeight) / 2;
+        // Offsets will be rounded to some precision level
+        // Otherwise they cause floating precission accuracies problem in javascript, which will later cause to get improper (Math.floor will round to a value lower by 1) calculations in converting coordinates to points and vice versa
+        const offsetX = dPR((currWidth - idealWidth) / 2);
+        const offsetY = dPR((currHeight - idealHeight) / 2);
 
         return [idealWidth, idealHeight, offsetX, offsetY];
     }
@@ -232,44 +351,32 @@ function Game(props) {
     }
 
     const renderLines = (clickedLines) => {
-        // TODO: somehow we should keep track of all lines
-        // then use that array to output lines in the right fashion
-        // <line x1={} y1={} x2={} y2={}/>
-
-        // activeLines is a dictionary
-        /*
-                00-01 02 03
-                10-11 12 13
-
-                0001: true
-                0010: nope
-                0111: nope
-                1011: true
-
-                i,j,i,j+1
-                i,j,i+1,j
-                i,j+1,i+1,j+1
-                i+1,j,i+1,j+1
-        */
         let lines = [];
-        // horizontal lines
-        for (let j = 0; j < rows; j++) {
-            for (let i = 0; i < cols - 1; i++) {
+        const linesPoints = history[history.length - 1].clickedLines;
+        Object.entries(linesPoints).forEach(([key, turnValue]) => {
+            const [px1, py1, px2, py2] = key.split('-').map((strnum) => parseInt(strnum));
+            const [lineX1, lineY1, lineX2, lineY2] = getLineCoordBetweenPoints([px1, py1, px2, py2]);
+            lines.push([lineX1, lineY1, lineX2, lineY2, turnValue]);
 
-            }
-        }
-        // vertical lines
+        })
 
-        const svgLines = null; // lines.map()
+        const svgLines = lines.map(([lineX1, lineY1, lineX2, lineY2, turnValue]) => {
+            return (
+                <line key={[lineX1, lineY1, lineX2, lineY2].join('-')} x1={lineX1} y1={lineY1} x2={lineX2} y2={lineY2} stroke={playerColors[turnValue]} />
+            );
+        });
 
+        // console.log("Debug renderlines svglines");
+        // console.log(svgLines);
+        return svgLines;
     }
 
     const renderDashedLine = () => {
         const [x1, y1, x2, y2] = [dashedLineCoord.x1, dashedLineCoord.y1, dashedLineCoord.x2, dashedLineCoord.y2];
 
-        if (x1 && y1 && x2 && y2) {
-            console.log("Dashed line is being rendered");
-            return (<line key="dashed-line" x1={x1} x2={x2} y1={y1} y2={y2} strokeDasharray="4" stroke="red" />);
+        if (![x1, y1, x2, y2].includes(null)) {
+            // console.log("Dashed line is being rendered");
+            return (<line key="dashed-line" x1={x1} x2={x2} y1={y1} y2={y2} strokeDasharray="4" stroke="grey" />);
         }
         // console.log("Dashed line NOT rendered");
         // console.log(dashedLineCoord);
@@ -283,9 +390,10 @@ function Game(props) {
             <p>window inner width {window.innerWidth}</p>
             <div id={props.boardId} className="svgDiv" ref={divRef} onMouseMove={handleMouseMove} onClick={handleMouseClick}>
                 <svg height="100%" width="100%" style={{ backgroundColor: "cyan" }} >
-                    {renderCircles()}
+                    {/* rendering order matters (circles/dots should be in front) */}
                     {renderDashedLine()}
                     {renderLines()}
+                    {renderCircles()}
                 </svg>
             </div>
             <p style={{ width: "50%" }}>In Game.js svgDiv width is {currWidth}</p>
@@ -303,4 +411,12 @@ function generatePlayersZeroScore(playerCount) {
         playersZeroScore['player' + i] = 0;
     }
     return playersZeroScore;
+}
+
+// (d)efault (P)recision (R)ounding method: this is to prevent floating pointing point inaccuracies and follow same precision level
+const dPR = (num) => {
+    // Math.ceil is a safer choice
+    // suppose 0.3333...3 is the MINIMUM THRESHOLD to reaching something
+    // 0.4 is more safer than 0.3, as 0.4 exceed MINIMUM THRESHOLD but 0.3 is below it.
+    return Math.ceil(num * 10) / 10;
 }
