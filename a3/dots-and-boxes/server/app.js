4f4ae0f10076590c8fde99b2310f864977b6d03b
Akashdeep Singh <akash02ita@gmail.com>
2022-12-24 00:27:41 -0700

    fix render.com failing deploy, one alternative last time was to use 0.0.0.0 ip address in app.listen now trying if setting up app.use for static files before server routs (gameRouter) works or not.

--- a3/dots-and-boxes/server/app.js
+++ a3/dots-and-boxes/server/app.js
@@ -8,11 +8,13 @@ const cookieParser = require('cookie-parser');
 app.use(cookieParser());
 app.use(express.json());
 
-app.use("/", gameRouter);
-
 // use static public folder: client -> npm run build
 const path = require('path');
 app.use(express.static(path.join(__dirname, "../client", "build")));
+
+app.use("/", gameRouter);
+
+// allow routes of react
 app.use((req, res, next) => {
     res.sendFile(path.join(__dirname, "../client", "build", "index.html"));
 });
